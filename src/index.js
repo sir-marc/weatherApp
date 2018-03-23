3e9d8c74ba716fde55e98083603063b55321f623
Marc <marc.berli@windowslive.com>
2018-03-23 14:41:16 +0100

    Revert "typo"
    This reverts commit 6c6ec7b293f37acd9bbdd777aea4737fbdba33e9.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/styles/inline.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./serviceworker.js')
-    .then(reg => console.log('whoo', reg));
-}
